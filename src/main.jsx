67f896222376813a7c32288807f5139f4d5edc85
Arjun KR <arjunkrishnaraj123@gmail.com>
2024-06-24 17:51:25 +0530

    added speed insight

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
+import App from './App';
+import './index.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Analytics />
+    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
